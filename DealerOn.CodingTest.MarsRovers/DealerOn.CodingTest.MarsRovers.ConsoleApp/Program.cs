a8106ece4e2545a0e94bb2cbcb426013ac98c62a
Danielle Isquierdo <danielle.isquierdo@gmail.com>
2021-08-26 14:38:37 -0300

    adding console app project

---
+++ DealerOn.CodingTest.MarsRovers/DealerOn.CodingTest.MarsRovers.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using DealerOn.CodingTest.MarsRovers.Domain;
+
+namespace DealerOn.CodingTest.MarsRovers.ConsoleApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter path to input file.");
+
+            var filePath = Console.ReadLine();
+
+            StreamReader sr = new StreamReader(filePath);
+
+            // assumption: only 2 rovers at a time for 1 plateau
+            var plateau = EntityCreator.CreatePlateau(sr.ReadLine());
+
+            // first rover
+            var rover1 = EntityCreator.CreateRover(sr.ReadLine(), plateau);
+            rover1.SendInstructions(sr.ReadLine());
+            Console.WriteLine(rover1.PrintCurrentPosition());
+
+            // second rover
+            var rover2 = EntityCreator.CreateRover(sr.ReadLine(), plateau);
+            rover2.SendInstructions(sr.ReadLine());
+            Console.WriteLine(rover2.PrintCurrentPosition());
+        }
+    }
+}
